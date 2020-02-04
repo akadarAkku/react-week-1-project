50d6bcf8b82fe82d7750ba331046ed6151922545
Akadar.akku <akadar.akku121@gmail.com>
2020-02-04 22:08:00 +0100

    wheather app done successfully

---
+++ build/precache-manifest.d6ad1448cc10fbe47e171eaabbd7c7e3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa19cd5f6e4185a7f1498ef42b33c920",
+    "url": "/wheather-app-gh-page/index.html"
+  },
+  {
+    "revision": "18f8eae197998080ba25",
+    "url": "/wheather-app-gh-page/static/css/2.58bd9c29.chunk.css"
+  },
+  {
+    "revision": "cc80fc9869c2f7e1de0a",
+    "url": "/wheather-app-gh-page/static/css/main.7c3e1ddc.chunk.css"
+  },
+  {
+    "revision": "18f8eae197998080ba25",
+    "url": "/wheather-app-gh-page/static/js/2.25bac13b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/wheather-app-gh-page/static/js/2.25bac13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc80fc9869c2f7e1de0a",
+    "url": "/wheather-app-gh-page/static/js/main.3383b0fa.chunk.js"
+  },
+  {
+    "revision": "79c0bf50f83c7353118f",
+    "url": "/wheather-app-gh-page/static/js/runtime-main.7e10b49b.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/wheather-app-gh-page/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/wheather-app-gh-page/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/wheather-app-gh-page/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/wheather-app-gh-page/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/wheather-app-gh-page/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/wheather-app-gh-page/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
