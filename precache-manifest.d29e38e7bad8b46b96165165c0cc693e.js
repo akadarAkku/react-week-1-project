37d6ffdcf42a61d88dcc35f25b6d665d2b3d0aa6
Akadar.akku <akadar.akku121@gmail.com>
2020-02-07 20:56:47 +0100

    Updates

---
+++ precache-manifest.d29e38e7bad8b46b96165165c0cc693e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf714ff051c0f40e84ee014e8e5814ba",
+    "url": "/week-1-project/index.html"
+  },
+  {
+    "revision": "3f389191d1ebac87c781",
+    "url": "/week-1-project/static/css/2.f11e8428.chunk.css"
+  },
+  {
+    "revision": "78bd2dbd2a6dcd3003f7",
+    "url": "/week-1-project/static/css/main.7a8e096b.chunk.css"
+  },
+  {
+    "revision": "3f389191d1ebac87c781",
+    "url": "/week-1-project/static/js/2.25bac13b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/week-1-project/static/js/2.25bac13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78bd2dbd2a6dcd3003f7",
+    "url": "/week-1-project/static/js/main.c821ecb3.chunk.js"
+  },
+  {
+    "revision": "055f2d337b6fd5233ce8",
+    "url": "/week-1-project/static/js/runtime-main.9bf635c6.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/week-1-project/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/week-1-project/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/week-1-project/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/week-1-project/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/week-1-project/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/week-1-project/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-week-1-project/precache-manifest.ac71e401de52f9bd1e176f294cbf3827.js"
+  "/week-1-project/precache-manifest.d29e38e7bad8b46b96165165c0cc693e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-week-1-project/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/week-1-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
